790feefe7e8e31ded704073b2511dbca8d7011fc
Minsha Moidheen <minumoidheen22@gmail.com>
2025-10-06 15:16:40 +0530

    urls added

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const BASE_URL = '/api'
+
+export const PARTY_URL =  '/parties'
+
+
+
--- src/store/api/baseApi.ts
+++ src/store/api/baseApi.ts
@@ -1,8 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { BASE_URL } from '@/constants'
 
 export const baseApi = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   tagTypes: ['Party'],
   endpoints: () => ({}),
 })
